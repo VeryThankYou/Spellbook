c300fddd68a84dfea25f65d9dc460ca7a3c036f5
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2020-03-24 09:24:52 +0100

    updateret et eller andet

--- gemDataModul.py
+++ gemDataModul.py
@@ -1,7 +1,12 @@
-import json
+import json, requests
 
 with open('data.json', 'r', encoding="utf-8") as f:
 	data = json.load(f)
 	f.close()
 
-print(data[0]['slots'])
+def prepSpell(string):
+    response = requests.get("https://api.open5e.com/spells/?name="+str(string))
+    result = json.load(response)
+    print(str(result['results'][0]['desc']))
+
+prepSpell("Acid Arrow")
