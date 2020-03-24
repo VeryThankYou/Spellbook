d7fbfff4048e53082f31bc917f001a2643cf1001
Vilijam <37106752+Vilijam@users.noreply.github.com>
2020-03-24 09:16:40 +0100

    Søgning fungerer* nu
    *det kunne være bedre

--- testetest.py
+++ testetest.py
@@ -2,26 +2,14 @@ import json
 import requests
 
 
-response = requests.get("api.open5e.com/spells/?dnd_class=Wizard")
+searchThis = input("Hvilken besværgelse vil du gerne finde?\n")
+response = requests.get("https://api.open5e.com/spells/?search="+searchThis)
 
 print(response)
+if response.status_code == 200:
+    ting1 = response.text
+    print(ting1)
 
 
 
-# def findSpell():
-#     searchedSpell = input("Which spell do you want to find? \n")
-#     spellName = searchedSpell.lower()
-#     spellName = spellName.title()
 
-#     if spellName in data.keys():
-#         return data[spellName]
-
-#     else:
-#         spellName = searchedSpell.replace(" ","")
-#         spellName = spellName.lower()
-#         spellName = spellName.title()
-#         if spellName in data.keys():
-#             return data[spellName]
-
-
-# print(findSpell())
