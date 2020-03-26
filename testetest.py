eed88159a7d7a4cd16b0f2c5232c272bf7c0a2b0
Vilijam <37106752+Vilijam@users.noreply.github.com>
2020-03-26 15:16:04 +0100

    Man kan nu søge bedre og såen

--- testetest.py
+++ testetest.py
@@ -7,9 +7,14 @@ response = requests.get("https://api.open5e.com/spells/?search="+searchThis)
 
 print(response)
 if response.status_code == 200:
-    ting1 = response.text
-    print(ting1)
-
-
-
-
+    meem = response.text
+    dic = json.loads(meem)
+    dic2 = dic.get("results")
+    spelz = []
+    for e in dic2:
+        spelz.append(e.get("name"))
+    for e in range(len(spelz)):
+        num = e+1
+        print(num, end = ". ")
+        print(spelz[e])
+    input("Which of these spells is l00k 4?\n" )
