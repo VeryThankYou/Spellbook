8a82efb03727f5eff5ac81327f668f870e60c9b7
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2020-04-01 11:25:04 +0200

    Lavet funktionerne lvlup setlvl mm

--- gemDataModul.py
+++ gemDataModul.py
@@ -5,21 +5,98 @@ with open('data.json', 'r', encoding="utf-8") as f:
     f.close()
 
 def prepSpell(string):
-    response = requests.get("https://api.open5e.com/spells/?name="+str(string))
+    response = requests.get("https://api.open5e.com/spells/?search="+str(string))
     response = response.json()
-    data[0]['prep'].append(response['results'][0]['name'])
-    with open('data.json', 'w', encoding="utf-8") as f:
-        json.dump(data, f)
-        f.close()
+    if len(response['results']) > 1:
+        n = 1
+        for e in response['results']:
+            print(str(n)+" - "+str(e['name']))
+            n += 1
+        print("What spell did you mean?")
+        inp = int(input("Choose spell by index number\n"))
+        spellname = response['results'][inp-1]['name']
+    else:
+        spellname = response['results'][0]['name']
+    known = 0
+    for e in data[0]['prep']:
+        if e == spellname:
+            known += 1
+    if known > 0:
+        print("You have prepared this spell already.")
+    else:
+        data[0]['prep'].append(str(spellname))
+        with open('data.json', 'w', encoding="utf-8") as f:
+            json.dump(data, f)
+            f.close()
+
+
 
 def learnSpell(string):
-    response = requests.get("https://api.open5e.com/spells/?name="+str(string))
+    response = requests.get("https://api.open5e.com/spells/?search="+str(string))
     response = response.json()
-    data[0]['know'].append(response['results'][0]['name'])
+    if len(response['results']) > 1:
+        n = 1
+        for e in response['results']:
+            print(str(n)+" - "+str(e['name']))
+            n += 1
+        print("What spell did you mean?")
+        inp = int(input("Choose spell by index number\n"))
+        spellname = response['results'][inp-1]['name']
+    else:
+        spellname = response['results'][0]['name']
+    known = 0
+    for e in data[0]['know']:
+        if e == spellname:
+            known += 1
+    if known > 0:
+        print("You have learned this spell already.")
+    else:
+        data[0]['know'].append(str(spellname))
+        with open('data.json', 'w', encoding="utf-8") as f:
+            json.dump(data, f)
+            f.close()
+
+def lvlup():
+    if int(data[0]['lvl']) >= 20:
+        print("You are already at max level")
+    else:
+        data[0]['lvl'] = int(data[0]['lvl']) + 1
+        with open('data.json', 'w', encoding="utf-8") as f:
+            json.dump(data, f)
+            f.close()
+
+def setlvl(i):
+    if int(i) < 1 or int(i) > 20:
+        print("Your input has to be within the level range 1-20")
+    else:
+        data[0]['lvl'] = int(i)
+        with open('data.json', 'w', encoding="utf-8") as f:
+            json.dump(data, f)
+            f.close()
+
+def longRest():
+    for e in data[0]['usedss']:
+        data[0]['usedss'][e] = 0
     with open('data.json', 'w', encoding="utf-8") as f:
-        json.dump(data, f)
-        f.close()
+            json.dump(data, f)
+            f.close()
 
-prepSpell("Acid Arrow")
+def useSpell(i):
+    if data[0]['usedss'][str(i)] >= data[0]['maxss'][str(i)]:
+        print("You have expended all spell slots of that level.")
+    else:
+        data[0]['usedss'][str(i)] = data[0]['usedss'][str(i)] + 1
+        with open('data.json', 'w', encoding="utf-8") as f:
+            json.dump(data, f)
+            f.close()    
 
+#prepSpell("Fireball")
+#learnSpell("acid Arrow")
 
+#learnSpell("eldri")
+
+#lvlup()
+#setlvl(2)
+#longRest()
+
+useSpell(1)
