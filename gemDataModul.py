c00dd817d7a5e9bb2b603e216901466bce0219a3
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2020-04-01 08:31:14 +0200

    Struktur tilføet med mere

--- gemDataModul.py
+++ gemDataModul.py
@@ -1,12 +1,25 @@
 import json, requests
 
 with open('data.json', 'r', encoding="utf-8") as f:
-	data = json.load(f)
-	f.close()
+    data = json.load(f)
+    f.close()
 
 def prepSpell(string):
     response = requests.get("https://api.open5e.com/spells/?name="+str(string))
-    result = json.load(response)
-    print(str(result['results'][0]['desc']))
+    response = response.json()
+    data[0]['prep'].append(response['results'][0]['name'])
+    with open('data.json', 'w', encoding="utf-8") as f:
+        json.dump(data, f)
+        f.close()
+
+def learnSpell(string):
+    response = requests.get("https://api.open5e.com/spells/?name="+str(string))
+    response = response.json()
+    data[0]['know'].append(response['results'][0]['name'])
+    with open('data.json', 'w', encoding="utf-8") as f:
+        json.dump(data, f)
+        f.close()
 
 prepSpell("Acid Arrow")
+
+
