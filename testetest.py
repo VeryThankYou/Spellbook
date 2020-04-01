2379aba099c9e63ad67def6604743c5e71ddb32c
Vilijam <37106752+Vilijam@users.noreply.github.com>
2020-04-01 10:00:48 +0200

    tranScribe
    function tranScribe er lavet til at printe en besværgelse.
    function findSpell er lavet til at finde en besværgelse.
    Ting til næste PUSH
    Cleric: Light<br/> Warlock: Archfey
    ^^ br burde ikke være der
    slug og document tamtam burde heller ikke printes

--- testetest.py
+++ testetest.py
@@ -1,33 +1,45 @@
 import json
 import requests
 
+def tranScribe(spelStuff):
+    for e in spelStuff:
+        print(e, end = "")
+        mRum = 24 - len(e)
+        for i in range(mRum):
+            print(" ", end = "")
+        print(":    ", end = "")
 
-searchThis = input("Hvilken besværgelse vil du gerne finde?\n")
-response = requests.get("https://api.open5e.com/spells/?search="+searchThis)
+        print(spelStuff.get(e))
 
-print(response)
-if response.status_code == 200:
-    meem = response.text
-    dic = json.loads(meem)
-    dic2 = dic.get("results")
-    if dic.get("count") > 1 and dic.get("count") < 11:
-        spelz = []
-        for e in dic2:
-            spelz.append(e.get("name"))
-        for e in range(len(spelz)):
-            num = e+1
-            print(num, end = ". ")
-            print(spelz[e])
-        ans = int(input("Which of these spells is l00k 4?\n" ))
-        print(spelz[ans])
+def findSpell():
+    searchThis = input("Hvilken besværgelse vil du gerne finde?\n")
+    response = requests.get("https://api.open5e.com/spells/?search="+searchThis)
+    if response.status_code == 200:
+        meem = response.text
+        dic = json.loads(meem)
+        dic2 = dic.get("results")
+        if dic.get("count") > 1 and dic.get("count") < 11:
+            spelz = []
+            for e in dic2:
+                spelz.append(e.get("name"))
+            for e in range(len(spelz)):
+                num = e+1
+                print(num, end = ". ")
+                print(spelz[e])
+            ans = int(input("Which of these spells is l00k 4?\n" ))
+            tranScribe(dic2[ans - 1])
 
-    elif dic.get("count") == 1:
-        lul = dic2[0]
-        print(lul.get("name"))
+        elif dic.get("count") == 1:
+            lul = dic2[0]
+            tranScribe(lul)
 
-    elif dic.get("count") == 0:
-        print("There are no spellzzz with this naeem\n")
-        
+        elif dic.get("count") == 0:
+            print("There are no spellzzz with this naeem\n")
+
+        else:
+            print("2 manyy spelzz braj, be spesifig, dyd\n")
     else:
-        print("2 manyy spelzz braj, be spesifig, dyd\n")
+        print("OOPSIE WOOPSE!! Uwu You made a fuckie wuckie!! A wittle fucko boingo! The code monkeys at our headquarteres are working VEWY HAWD to fix this! JK do it yourself")
+        print(response.status_code)
 
+findSpell()
