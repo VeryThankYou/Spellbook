4fd4430f44cceb374aa39692866e9fa014cfb76c
Vilijam <37106752+Vilijam@users.noreply.github.com>
2020-04-01 09:03:13 +0200

    Search THIS
    Man kan nu søge og finde en specifik besværgelse. Der er endda også lidt  bRuGeRvEnLiGhEd

--- testetest.py
+++ testetest.py
@@ -10,11 +10,24 @@ if response.status_code == 200:
     meem = response.text
     dic = json.loads(meem)
     dic2 = dic.get("results")
-    spelz = []
-    for e in dic2:
-        spelz.append(e.get("name"))
-    for e in range(len(spelz)):
-        num = e+1
-        print(num, end = ". ")
-        print(spelz[e])
-    input("Which of these spells is l00k 4?\n" )
+    if dic.get("count") > 1 and dic.get("count") < 11:
+        spelz = []
+        for e in dic2:
+            spelz.append(e.get("name"))
+        for e in range(len(spelz)):
+            num = e+1
+            print(num, end = ". ")
+            print(spelz[e])
+        ans = int(input("Which of these spells is l00k 4?\n" ))
+        print(spelz[ans])
+
+    elif dic.get("count") == 1:
+        lul = dic2[0]
+        print(lul.get("name"))
+
+    elif dic.get("count") == 0:
+        print("There are no spellzzz with this naeem\n")
+        
+    else:
+        print("2 manyy spelzz braj, be spesifig, dyd\n")
+
