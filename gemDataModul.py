da6648e688dab50fff5b3a95f872234b41f24838
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2020-04-20 15:07:21 +0200

    addchar

--- gemDataModul.py
+++ gemDataModul.py
@@ -11,6 +11,7 @@ def updateData():
     return data
 
 def prepSpell(string):
+    #Her defineres funktionen prepSpell, som tilføjer 
     response = requests.get("https://api.open5e.com/spells/?search="+str(string))
     response = response.json()
     if len(response['results']) > 1:
@@ -30,7 +31,7 @@ def prepSpell(string):
     if known > 0:
         print("You have prepared this spell already.")
     else:
-        data[0]['prep'].append(str(spellname))
+        data[charnumber]['prep'].append(str(spellname))
         with open('data.json', 'w', encoding="utf-8") as f:
             json.dump(data, f)
             f.close()
@@ -51,22 +52,22 @@ def learnSpell(string):
     else:
         spellname = response['results'][0]['name']
     known = 0
-    for e in data[0]['know']:
+    for e in data[charnumber]['know']:
         if e == spellname:
             known += 1
     if known > 0:
         print("You have learned this spell already.")
     else:
-        data[0]['know'].append(str(spellname))
+        data[charnumber]['know'].append(str(spellname))
         with open('data.json', 'w', encoding="utf-8") as f:
             json.dump(data, f)
             f.close()
 
 def lvlup():
-    if int(data[0]['lvl']) >= 20:
+    if int(data[charnumber]['lvl']) >= 20:
         print("You are already at max level")
     else:
-        data[0]['lvl'] = int(data[0]['lvl']) + 1
+        data[charnumber]['lvl'] = int(data[charnumber]['lvl']) + 1
         with open('data.json', 'w', encoding="utf-8") as f:
             json.dump(data, f)
             f.close()
@@ -75,36 +76,36 @@ def setlvl(i):
     if int(i) < 1 or int(i) > 20:
         print("Your input has to be within the level range 1-20")
     else:
-        data[0]['lvl'] = int(i)
+        data[charnumber]['lvl'] = int(i)
         with open('data.json', 'w', encoding="utf-8") as f:
             json.dump(data, f)
             f.close()
 
 def longRest():
-    for e in data[0]['usedss']:
-        data[0]['usedss'][e] = 0
+    for e in data[charnumber]['usedss']:
+        data[charnumber]['usedss'][e] = 0
     with open('data.json', 'w', encoding="utf-8") as f:
             json.dump(data, f)
             f.close()
 
 def useSpell(i):
-    if data[0]['usedss'][str(i)] >= data[0]['maxss'][str(i)]:
+    if data[charnumber]['usedss'][str(i)] >= data[charnumber]['maxss'][str(i)]:
         print("You have expended all spell slots of that level.")
     else:
-        data[0]['usedss'][str(i)] = data[0]['usedss'][str(i)] + 1
+        data[charnumber]['usedss'][str(i)] = data[charnumber]['usedss'][str(i)] + 1
         with open('data.json', 'w', encoding="utf-8") as f:
             json.dump(data, f)
             f.close()    
 
 def addSpellSlot(i):
-    data[0]['maxss'][str(i)] += 1
+    data[charnumber]['maxss'][str(i)] += 1
     with open('data.json', 'w', encoding="utf-8") as f:
             json.dump(data, f)
             f.close()
 
 def setSpellSlot(i):
     numslot = input("How many spell slots of this level do you have?\n")
-    data[0]['maxss'][str(i)] = int(numslot)
+    data[charnumber]['maxss'][str(i)] = int(numslot)
     with open('data.json', 'w', encoding="utf-8") as f:
             json.dump(data, f)
             f.close()
@@ -113,22 +114,35 @@ def updateSpellSlots():
     with open('slotdata.json', 'r', encoding="utf-8") as f:
         wizdict = json.load(f)
         f.close()
-    lvl = data[0]['lvl']
+    lvl = data[charnumber]['lvl']
     tal = 1
     for e in wizdict[0][str(lvl)]:
-        data[0]['maxss'][str(tal)] = wizdict[0][str(lvl)][e]
+        data[charnumber]['maxss'][str(tal)] = wizdict[0][str(lvl)][e]
         tal += 1
     with open('data.json', 'w', encoding="utf-8") as f:
         json.dump(data, f)
         f.close()
 
+def addChar(name):
+    with open('struktur.json', 'r', encoding="utf-8") as f:
+        structure = json.load(f)
+        f.close()
+    structure[0]['name'] = name
+    data.append(structure[0])
+    with open('data.json', 'w', encoding="utf-8") as f:
+        json.dump(data, f)
+        f.close()
+    
+
+
      
 data = updateData()
+charnumber = 1
 #prepSpell("Fireball")
 #learnSpell("acid Arrow")
 #learnSpell("eldri")
 #lvlup()
-setlvl(1)
+setlvl(13)
 #longRest()
 #useSpell(1)
 #addSpellSlot(3)
